95916343cfb5de45cd8db2e3bb89e4c42a8110c4
Solee Lee <carolinesoleelee@gmail.com>
2018-09-21 03:05:54 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,10 @@
 def using_times
-	#your code here
+	looping_string = 0
+  loop do
+    looping_string += 1
+    puts "Wingardium Leviosa"
+  if looping_string >= 7
+    break
 end
-
-
-
-
+end
+end
